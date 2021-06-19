e9d5f905a97b6b0680c5ff27c7671a3bfefd3dbc
hsappati <sappati.harish@gmail.com>
2021-06-19 21:07:28 +0530

    Third push_Harish

--- src/test/java/com/OrangeHRM/testcase.java
+++ src/test/java/com/OrangeHRM/testcase.java
@@ -4,7 +4,8 @@ public class testcase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Second push");
+		System.out.println("Third push");
+		System.out.println("Harish test");
 	}
 
 }
