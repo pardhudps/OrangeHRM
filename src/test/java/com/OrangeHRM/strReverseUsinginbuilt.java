ccc6c2602364afd2704f7aa67966c923bbbc1634
lagopavar <kumargopavarapu4@gmail.com>
2022-07-05 15:46:16 +0530

    Sample Java prgm 2

---
+++ src/test/java/com/OrangeHRM/strReverseUsinginbuilt.java
@@ -0,0 +1,12 @@
+package com.OrangeHRM;
+
+public class strReverseUsinginbuilt {
+	public static void main(String args[]) {
+		String str= "Automation";
+		StringBuilder str2 = new StringBuilder();
+		str2.append(str);
+		str2.reverse();
+		System.out.println(str2);
+	}
+
+}
