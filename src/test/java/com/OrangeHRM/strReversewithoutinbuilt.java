bf67ef7a1a2da2e867f3ea41b966f7453d542d5a
lagopavar <kumargopavarapu4@gmail.com>
2022-07-05 15:54:58 +0530

    Create strReversewithoutinbuilt.java

---
+++ src/test/java/com/OrangeHRM/strReversewithoutinbuilt.java
@@ -0,0 +1,16 @@
+package com.OrangeHRM;
+
+public class strReversewithoutinbuilt {
+	public static void main(String args[]) {
+		String str= "Lavanya kumar";
+		char chars[] = str.toCharArray();
+		for(int i = chars.length-1; i>=0; i-- ) {
+			System.out.print(chars[i]);
+		}
+		
+		
+}
+	
+}
+
+
